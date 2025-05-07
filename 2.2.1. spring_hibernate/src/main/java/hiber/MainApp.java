bcf9cda8005bf842cbc9873e6d72214f42fceb09
jaraslav <Jrikygzz@gmail.com>
2025-05-07 14:18:22 +0300

    ()

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -15,10 +15,14 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru", new Car("Volvo", 40)));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru", new Car("Audi", 8)));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru", new Car("BMW", 5)));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru", new Car("Hyundai", 20)));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",
+              new Car("Volvo", 40)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",
+              new Car("Audi", 8)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",
+              new Car("BMW", 5)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",
+              new Car("Hyundai", 20)));
 
 
       List<User> users = userService.listUsers();
--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -28,7 +28,7 @@ public class User {
 
    public User() {}
    
-   public User(String firstName, String lastName, String email,@Autowired Car car) {
+   public User(String firstName, String lastName, String email, Car car) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
